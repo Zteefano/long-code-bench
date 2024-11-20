3474a946d00cdbd72d4908bc31027b9b35b96b64
Stefano Rando <stefano.rando.c@protonmail.com>
2024-11-19 17:04:37 -0800

    chore: update test.py with desired functionality
    This commit updates the test.py file to include a desired behavior from
    the repository, which is to be able to run a Jamba model.

--- test.py
+++ test.py
@@ -1,43 +1,25 @@
 import json
-import os
-from src.long_code_bench.models import APIModel, OpenSourceModel
 
-# Load API keys from keys.json
-with open("keys.json", "r") as f:
-    keys = json.load(f)
+from datasets import load_from_disk
 
-# Prompt for testing
-prompt = "Explain the significance of the Renaissance period."
+from src.long_code_bench.inference.hf_eval import DatasetsEvaluator
+from src.long_code_bench.models import OpenSourceModel
 
-# Specify model type and version
-model_type = "llama"  #"anthropic" "llama" "openai"
+if __name__ == "__main__":
+	dataset = load_from_disk(
+		"data/swe_bench_small_text/SWE-bench__style-2__fs-all"
+	)
 
-# For OpenAI and Anthropic models, specify the model version
-model_version = "gpt-4" if model_type == "openai" else "claude-2"  # "gpt-4" "claude-2" "gpt-3.5-turbo"
+	with open("keys.json", "r") as f:
+		hf_key = json.load(f)["huggingface"]
+	model = OpenSourceModel("ai21labs/Jamba-tiny-dev", token=hf_key)
 
-# Specify the Hugging Face path for the desired LLaMA model
-hf_path = "meta-llama/Llama-2-13b-hf"  # "meta-llama/Llama-2-13b-hf" "meta-llama/Llama-2-7b-hf"
-
-if model_type in ["openai", "anthropic"]:
-    # Get the API key
-    api_key = keys.get(model_type)
-    if not api_key:
-        raise ValueError(f"No API key found for model type: {model_type}")
-
-    model = APIModel(model_type=model_type, model_version=model_version, api_key=api_key)
-
-elif model_type == "llama":    
-    # Get the API key
-    hf_token = keys.get("huggingface")
-    model = OpenSourceModel(hf_path=hf_path, token=hf_token)
-
-else:
-    raise ValueError(f"Unsupported model type: {model_type}")
-
-# Generate and print text based on the prompt
-try:
-    response = model.generate(prompt)
-    print("Generated Text:")
-    print(response)
-except Exception as e:
-    print(f"An error occurred: {e}")
+	evaluator = DatasetsEvaluator(
+		model,
+		dataset,
+		"text",
+		"results/swe_bench_small.json",
+		max_context_length=8_192,
+		max_output_length=8_192,
+	)
+	evaluator.run()
